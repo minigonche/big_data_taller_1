290d9d0b7b9d749d3a70a9ce02da04b8a47fef57
Felipe Gonzalez Casabianca <f.gonzalez1899@uniandes.edu.co>
2018-09-18 12:27:35 -0500

    More changes

--- website/app1/RF/RF1.py
+++ website/app1/RF/RF1.py
@@ -2,10 +2,12 @@
 # Requerimiento funcional 1
 from django.http import HttpResponse
 import requests
+from django.shortcuts import render
 
 def hacer_requerimiento(request):
 
 	#TODO
+	data = {}
 
 	# Franja Horaria
 	hora_inicio = request.POST.get('RF1_hora_inicio')
@@ -21,6 +23,17 @@ def hacer_requerimiento(request):
 	hora_final_fin = int(hora_fin)
 	if(merid_fin == 'PM'):
 		hora_final_fin = hora_final_fin + 12
+
+
+	#time strings
+	hora_inicio_string = hora_inicio + ':00' + merid_inicio.lower()
+	hora_fin_string = hora_fin + ':00' + merid_fin.lower()
+
+	
+	#REVISA SI LA FRANJA HORARIA ES CORRECTA
+	if(hora_final_inicio >= hora_final_fin):
+		data['error'] = 'Favor inserte una franja horaria valida. Franja recibida: ' + hora_inicio_string + ' a ' + hora_fin_string + '.'
+		return render(request, 'app1/error.html', data)
 
 	html = "Hora Inicio: " + str(hora_final_inicio) + ':00 '
 	html = html + "Hora Fin: " + str(hora_final_fin) + ':00 '
--- website/app1/RF/RF4.py
+++ website/app1/RF/RF4.py
@@ -2,10 +2,12 @@
 # Requerimiento funcional 1
 from django.http import HttpResponse
 import requests
+from django.shortcuts import render
 
 def hacer_requerimiento(request):
 
 	#TODO
+	data = {}
 
 	# Franja Horaria
 	hora_inicio = request.POST.get('RF4_hora_inicio')
@@ -21,6 +23,17 @@ def hacer_requerimiento(request):
 	hora_final_fin = int(hora_fin)
 	if(merid_fin == 'PM'):
 		hora_final_fin = hora_final_fin + 12
+
+
+	#time strings
+	hora_inicio_string = hora_inicio + ':00' + merid_inicio.lower()
+	hora_fin_string = hora_fin + ':00' + merid_fin.lower()
+
+	
+	#REVISA SI LA FRANJA HORARIA ES CORRECTA
+	if(hora_final_inicio >= hora_final_fin):
+		data['error'] = 'Favor inserte una franja horaria valida. Franja recibida: ' + hora_inicio_string + ' a ' + hora_fin_string + '.'
+		return render(request, 'app1/error.html', data)
 
 	# Mes
 	dia_semana = request.POST.get('RF4_dia_semana')
