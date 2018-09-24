c30d430e499499fdea43845f601112dd0266ed00
andrea <ap3rg@virginia.edu>
2018-09-24 09:35:13 -0500

    Updated RF1_mapper

--- website/app1/RA/RA2.py
+++ website/app1/RA/RA2.py
@@ -6,6 +6,8 @@ import requests
 import numpy as np
 import copy
 import json
+from paramiko import SSHClient
+from scp import SCPClient
 
 def hacer_requerimiento(request):
 
@@ -126,8 +128,8 @@ def get_data():
 
 	Returns: array of lines
 	"""
-	#return(get_remote_data())
-	return(get_local_data())
+	return(get_remote_data())
+	#return(get_local_data())
 
 
 def get_local_data():
@@ -142,8 +144,6 @@ def get_local_data():
 		return(f.readlines())
 
 
-
-
 def get_remote_data():
 	"""
 	Loads the data from local. Ment for testing
@@ -151,7 +151,29 @@ def get_remote_data():
 	Returns: array of strings
 		Each string corresponds to a line
 	"""
-	raise Error('Not Implemented yet')
+
+
+	host = 'bigdata-cluster1-03.virtual.uniandes.edu.co'
+	port = 22
+	usr = 'bigdata07'
+	pwd = '969ba5d6f576c2c4377f2381d2829207'
+
+	ssh = SSHClient()
+	ssh.load_system_host_keys()
+	ssh.connect(host, port, usr, pwd)
+
+
+	# SCPCLient takes a paramiko transport as its only argument
+	scp = SCPClient(ssh.get_transport())
+
+	scp.get(remote_path = 'results/RA2/part-00000',
+		local_path= 'app1/RA/received_data/RA2/')
+
+	scp.close()
+
+	with open('app1/RA/received_data/RA2/part-00000') as f:
+		return(f.readlines())
+
 
 
 
