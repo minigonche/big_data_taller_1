9ebea9c48d0172332e59da5cb834c604bd4ac5b7
Felipe Gonzalez Casabianca <f.gonzalez1899@uniandes.edu.co>
2018-11-03 07:45:11 -0500

    Progreso en polaridad

--- website/app2/Requerimientos/Polaridad.py
+++ website/app2/Requerimientos/Polaridad.py
@@ -1,9 +1,50 @@
 #Polaridad
 
 from django.shortcuts import render
+import pymongo
+
+def hacer_requerimiento(request, mongo_db):
+
+    nombre_col = "polaridad"
+    max_display = 200
+    data = {}
+    #Extrae los elementos tipicos
+
+    #Polaridad
+    polaridad = {'negativo':-1, 'neutro':0, 'positivo':1}
+    data['polaridad'] = {}
+    for pol in polaridad.keys():
+        data['polaridad'][pol] = {"screen_name":"Ninguno", "full_text":"Sin Texto"}
+
+        list = mongo_db[nombre_col].aggregate([{"$match": {"polaridad": polaridad[pol]}},{"$sample": {"size": 1}} ])
+
+        for pos in list:
+            screen_name = pos["user"]["screen_name"]
+            full_text = pos["full_text"]
+            if(len(full_text) > max_display):
+                full_text = full_text[:max_display] + "..."
+
+            data['polaridad'][pol] = {"screen_name":screen_name, "full_text":full_text}
 
 
-def hacer_requerimiento(request):
+    #Sexismo
+    sexismo = {'machista_violento':-2, 'machista':1, 'neutro':0, 'feminista':1, "feminista_violento":2}
+    data['sexismo'] = {}
 
-    # TODO
-    return render(request, 'app2/Polaridad.html', None)
+    for sex in sexismo.keys():
+
+
+        data['sexismo'][sex] =  {"screen_name":"Ninguno", "full_text":"Sin Texto"}
+        list = mongo_db[nombre_col].aggregate([{"$match": {"sexismo": sexismo[sex]}},{"$sample": {"size": 1}} ])
+
+        for pos in list:
+            screen_name = pos["user"]["screen_name"]
+            full_text = pos["full_text"]
+            if(len(full_text) > max_display):
+                full_text = full_text[:max_display] + "..."
+
+            data['sexismo'][sex] = {"screen_name":screen_name, "full_text":full_text}
+
+
+    print(data)
+    return render(request, 'app2/Polaridad.html', data)
--- website/app2/views.py
+++ website/app2/views.py
@@ -6,16 +6,34 @@ from app2.Requerimientos import Polaridad as pol
 from app2.Requerimientos import Historico as his
 from app2.Requerimientos import Matoneo as mat
 
+import json
+import pymongo
+
 
 # Index view
 def index(request):
     return render(request, 'app2/index.html', None)
 
 def Polaridad(request):
-	return pol.hacer_requerimiento(request)
+	return pol.hacer_requerimiento(request, dar_base_de_datos())
 
 def Historico(request):
-	return his.hacer_requerimiento(request)
+	return his.hacer_requerimiento(request, dar_base_de_datos())
 
 def Matoneo(request):
-	return mat.hacer_requerimiento(request)
+	return mat.hacer_requerimiento(request, dar_base_de_datos())
+
+
+
+
+def dar_base_de_datos():
+    with open('app2/static/app2/jsons/db_configuration.json','r') as f:
+        data = json.load(f)
+
+        print("mongodb://" + data['client_location'] + "/")
+        client = pymongo.MongoClient("mongodb://" + data['client_location'] + "/")
+        mydb = client[data["db_name"]]
+
+        return(mydb)
+
+    raise("Hibo un error conectando a la base de datos")
